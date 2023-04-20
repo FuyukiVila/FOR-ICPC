237aecc376715b8ab7806e92c0c690b4eb5d6334
Fuyuki_Vila <1642421711@qq.com>
2023-04-20 23:01:10 +0800

    2023.4.20

--- PTA/TianTi_Training/L2-007.cpp
+++ PTA/TianTi_Training/L2-007.cpp
@@ -112,29 +112,34 @@ void get_primes(int n) {
 }
 
 struct family {
-    string father_id, mother_id;
     double cnt_house;
     double square;
+
+    family() : cnt_house(0), square(0) {}
 };
 
 struct node {
-    string min_id;
-    int cnt;
-    double cnt_house;
-    double square;
+    string min_id{"99999"};
+    int cnt{};
+    double cnt_house{};
+    double square{};
 
     bool operator<(const node &b) const {
-        if (square == b.square) {
+        if (square / cnt == b.square / b.cnt) {
             return min_id < b.min_id;
         }
-        return square > b.square;
+        return square / cnt > b.square / b.cnt;
     }
 };
 
-unordered_map<string,string>grp;
+unordered_map<string, string> grp;
+map<string, node> m;
+map<string, family> f;
+set<string> people;
+set<node> ans;
 
-string find(string a){
-    if(grp[a]==a) return a;
+string find(string a) {
+    if (grp[a] == a) return a;
     return grp[a] = find(grp[a]);
 }
 
@@ -143,8 +148,39 @@ auto solve() {
     cin >> n;
     cout << fixed << setprecision(3);
     while (n--) {
-        string id;
-        cin>>id;
+        string id, father_id, mother_id;
+        cin >> id >> father_id >> mother_id;
+        vector<string> v;
+        v.emplace_back(id);
+        if (father_id != "-1")v.emplace_back(father_id);
+        if (mother_id != "-1")v.emplace_back(mother_id);
+        int k;
+        cin >> k;
+        for (int i = 1; i <= k; i++) {
+            string son;
+            cin >> son;
+            v.emplace_back(son);
+        }
+        cin >> f[id].cnt_house >> f[id].square;
+//        sort(v.begin(), v.end());
+        for (const auto &x: v) {
+            people.insert(x);
+            if (grp.find(x) == grp.end())grp[x] = x;
+            grp[find(x)] = find(v[0]);
+        }
+    }
+    for (const auto &x: people) {
+        m[find(x)].cnt++;
+        m[find(x)].min_id = min(x, m[find(x)].min_id);
+        m[find(x)].square += f[x].square;
+        m[find(x)].cnt_house += f[x].cnt_house;
+    }
+    for (const auto &[i, j]: m) {
+        ans.insert(j);
+    }
+    cout<<ans.size()<<'\n';
+    for (const auto &[id, cnt, house, sq]: ans) {
+        cout << id << ' ' << cnt << ' ' << house / cnt << ' ' << sq / cnt << '\n';
     }
 }
 
