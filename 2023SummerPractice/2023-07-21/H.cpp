87673cd00e6a2def779e3faad4f3267fff246c20
Fuyuki_Vila <1642421711@qq.com>
2023-07-21 19:23:08 +0800

    update

---
+++ 2023SummerPractice/2023-07-21/H.cpp
@@ -0,0 +1,78 @@
+#define GKD std::cin.tie(nullptr)->std::ios::sync_with_stdio(false)
+#define clr(a, b) memset(a, b, sizeof(a))
+
+#include <bits/stdc++.h>
+
+#define dbg(x...) \
+    do { \
+        std::cout << #x << " -> "; \
+        err(x); \
+    } while (0)
+
+void err() {
+    std::cout << std::endl;
+}
+
+template<class T, class... Ts>
+void err(T arg, Ts &... args) {
+    std::cout << arg << ' ';
+    err(args...);
+}
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+const double pi = acos(-1);
+const int INF = 0x3f3f3f3f;
+const ll mod = 2;
+
+template<typename T>
+inline constexpr T qpow(T a, T n) {
+    T ans = 1;
+    while (n) {
+        if (n & 1) ans = (ans * a) % mod;
+        n >>= 1;
+        a = (a * a) % mod;
+    }
+    return ans;
+}
+
+constexpr int N = 1e7 + 100;
+int minp[N];
+vector<int> primes;
+bool st[N];
+
+void get_primes(int n) {
+    for (int i = 2; i <= n; i++) {
+        if (!st[i]) minp[i] = i, primes.emplace_back(i);
+        for (int j = 0; primes[j] * i <= n; j++) {
+            int t = primes[j] * i;
+            st[t] = true;
+            minp[t] = primes[j];
+            if (i % primes[j] == 0) break;
+        }
+    }
+}
+
+auto genshin_start() {
+    ll n, r, b, g;
+    cin >> n >> r >> g >> b;
+    int res = (r + g + b);
+    if(n % 2 == 0) {
+        cout << "Alice\n";
+    }else{
+        if(res % 2 == 1){
+            cout << "Alice\n";
+        }else{
+            cout << "Bob\n";
+        }
+    }
+}
+
+signed main() {
+    GKD;
+    auto T{1};
+    cin >> T;
+    while (T--) genshin_start();
+    return 0;
+}
