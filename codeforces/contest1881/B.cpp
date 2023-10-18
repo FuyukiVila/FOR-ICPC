d23b803bcdd68ab031bdcb80cd59beb11bcbc085
FuyukiVila <1642421711@qq.com>
2023-10-18 20:54:15 +0800

    update

---
+++ codeforces/contest1881/B.cpp
@@ -0,0 +1,86 @@
+#define GKD std::cin.tie(nullptr)->std::ios::sync_with_stdio(false)
+#define clr(a, b) memset(a, b, sizeof(a))
+
+#include <bits/stdc++.h>
+
+#define dbg(x...) \
+    do { \
+        std::cout << #x << " -> "; \
+        err(x); \
+    } while (0)
+
+void err() {
+    std::cout << std::endl;
+}
+
+template<class T, class... Ts>
+void err(T arg, Ts &... args) {
+    std::cout << arg << ' ';
+    err(args...);
+}
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+template<typename T>
+using umap = unordered_map<T, T>;
+template<typename T>
+using uset = unordered_set<T, T>;
+const double pi = acos(-1);
+const int INF = 0x3f3f3f3f;
+const int mod = 0;
+
+inline constexpr ll qpow(ll _a, ll _n, ll _mod = mod) {
+    ll ans = 1;
+    while (_n) {
+        if (_n & 1) ans *= _a;
+        _n >>= 1;
+        _a *= _a;
+        if (_mod > 0) {
+            ans %= _mod;
+            _a %= _mod;
+        }
+    }
+    return ans;
+}
+
+constexpr int N = 1e7 + 100;
+int minp[N];
+vector<int> primes;
+bool st[N];
+
+void get_primes(int n) {
+    for (int i = 2; i <= n; i++) {
+        if (!st[i]) minp[i] = i, primes.emplace_back(i);
+        for (int j = 0; primes[j] * i <= n; j++) {
+            int t = primes[j] * i;
+            st[t] = true;
+            minp[t] = primes[j];
+            if (i % primes[j] == 0) break;
+        }
+    }
+}
+
+std::random_device rd;
+std::default_random_engine eng(rd());
+std::uniform_int_distribution<ll> ranint(1, 1e18);
+
+//玩原神导致的
+void genshin_start() {
+    ll x, y, z;
+    cin >> x >> y >> z;
+    ll k = __gcd(__gcd(x, y), z);
+    if (x / k + y / k + z / k - 3 <= 3) {
+        cout << "yes\n";
+    } else {
+        cout << "no\n";
+    }
+}
+
+signed main() {
+    GKD;
+    auto T = 1;
+    cin >> T;
+    while (T--) genshin_start();
+    return 0;
+}
---
+++ codeforces/contest1881/C.cpp
@@ -0,0 +1,96 @@
+#define GKD std::cin.tie(nullptr)->std::ios::sync_with_stdio(false)
+#define clr(a, b) memset(a, b, sizeof(a))
+
+#include <bits/stdc++.h>
+
+#define dbg(x...) \
+    do { \
+        std::cout << #x << " -> "; \
+        err(x); \
+    } while (0)
+
+void err() {
+    std::cout << std::endl;
+}
+
+template<class T, class... Ts>
+void err(T arg, Ts &... args) {
+    std::cout << arg << ' ';
+    err(args...);
+}
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+template<typename T>
+using umap = unordered_map<T, T>;
+template<typename T>
+using uset = unordered_set<T, T>;
+const double pi = acos(-1);
+const int INF = 0x3f3f3f3f;
+const int mod = 0;
+
+inline constexpr ll qpow(ll _a, ll _n, ll _mod = mod) {
+    ll ans = 1;
+    while (_n) {
+        if (_n & 1) ans *= _a;
+        _n >>= 1;
+        _a *= _a;
+        if (_mod > 0) {
+            ans %= _mod;
+            _a %= _mod;
+        }
+    }
+    return ans;
+}
+
+constexpr int N = 1e7 + 100;
+int minp[N];
+vector<int> primes;
+bool st[N];
+
+void get_primes(int n) {
+    for (int i = 2; i <= n; i++) {
+        if (!st[i]) minp[i] = i, primes.emplace_back(i);
+        for (int j = 0; primes[j] * i <= n; j++) {
+            int t = primes[j] * i;
+            st[t] = true;
+            minp[t] = primes[j];
+            if (i % primes[j] == 0) break;
+        }
+    }
+}
+
+std::random_device rd;
+std::default_random_engine eng(rd());
+std::uniform_int_distribution<ll> ranint(1, 1e18);
+
+//玩原神导致的
+string a[1000];
+
+void genshin_start() {
+    int n;
+    cin >> n;
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            char c[] = {a[i][j], a[j][n - i - 1], a[n - i - 1][n - j - 1], a[n - j - 1][i]};
+            char s = *max_element(c, c + 4);
+            for (int k = 0; k < 4; k++) {
+                ans += s - c[k];
+            }
+        }
+    }
+    cout << ans / 4 << '\n';
+}
+
+signed main() {
+    GKD;
+    auto T = 1;
+    cin >> T;
+    while (T--) genshin_start();
+    return 0;
+}
