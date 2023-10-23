0a943dd59104783d1dc06f3668275c47b164a8a5
FuyukiVila <1642421711@qq.com>
2023-10-23 21:03:09 +0800

    update

--- 板子/线段树.cpp
+++ 板子/线段树.cpp
@@ -1,79 +1,70 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-template <typename T>
+template<typename T>
 class SegTreeLazyRangeAdd {
-    vector<T> tree, lazy;
-    vector<T> *arr;
-    int n, root, n4, end;
-
-    void maintain(int cl, int cr, int p) {
-        int cm = cl + (cr - cl) / 2;
-        if (cl != cr && lazy[p]) {
-            lazy[p * 2] += lazy[p];
-            lazy[p * 2 + 1] += lazy[p];
-            tree[p * 2] += lazy[p] * (cm - cl + 1);
-            tree[p * 2 + 1] += lazy[p] * (cr - cm);
-            lazy[p] = 0;
-        }
-    }
-
-    T range_sum(int l, int r, int cl, int cr, int p) {
-        if (l <= cl && cr <= r) return tree[p];
-        int m = cl + (cr - cl) / 2;
-        T sum = 0;
-        maintain(cl, cr, p);
-        if (l <= m) sum += range_sum(l, r, cl, m, p * 2);
-        if (r > m) sum += range_sum(l, r, m + 1, cr, p * 2 + 1);
-        return sum;
-    }
-
-    void range_add(int l, int r, T val, int cl, int cr, int p) {
-        if (l <= cl && cr <= r) {
-            lazy[p] += val;
-            tree[p] += (cr - cl + 1) * val;
-            return;
-        }
-        int m = cl + (cr - cl) / 2;
-        maintain(cl, cr, p);
-        if (l <= m) range_add(l, r, val, cl, m, p * 2);
-        if (r > m) range_add(l, r, val, m + 1, cr, p * 2 + 1);
-        tree[p] = tree[p * 2] + tree[p * 2 + 1];
-    }
-
-    void build(int s, int t, int p) {
-        if (s == t) {
-            tree[p] = (*arr)[s];
-            return;
-        }
-        int m = s + (t - s) / 2;
-        build(s, m, p * 2);
-        build(m + 1, t, p * 2 + 1);
-        tree[p] = tree[p * 2] + tree[p * 2 + 1];
-    }
-
 public:
-    explicit SegTreeLazyRangeAdd<T>(vector<T> v) {
+    struct node {
+        T value{};
+        int add{};
+        int change{};
+        size_t l{}, r{};
+    };
+    vector<node> tree;
+    static size_t n, n4;
+
+    void push_up(size_t x) {
+        //push up操作
+        //区间求和
+        tree[x].value = tree[2 * x].value + tree[2 * x + 1].value;
+//        区间求max
+//        tree[x].value = max(tree[2 * x].value, tree[2 * x + 1].value);
+    }
+
+    void push_down(size_t x) {
+        //push down操作
+        //区间求和
+        if (tree[x].change) {
+            tree[x * 2].change = tree[x * 2 + 1].change = tree[x].change;
+            tree[x].value += tree[x].change * (tree[x].r - tree[x].l + 1);
+        }
+        if (tree[x].add) {
+            tree[x * 2].add = tree[x * 2 + 1].add = tree[x].add;
+            tree[x].value += tree[x].add * (tree[x].r - tree[x].l + 1);
+        }
+        //区间求max
+//        if (tree[x].change) {
+//            tree[x * 2].change = tree[x * 2 + 1].change = tree[x].change;
+//            tree[x].value = tree[x].change;
+//        }
+//        if (tree[x].add) {
+//            tree[x * 2].add = tree[x * 2 + 1].add = tree[x].add;
+//            tree[x].value += tree[x].add;
+//        }
+
+        tree[x].change = tree[x].add = 0;
+
+    }
+
+    void build(vector<T> &v, int x = 1, int l = 1, int r = n) {
+        if (l == r) {
+            tree[x].value = v[l];
+            tree[x].l = tree[x].r = l;
+            return;
+        }
+        int m = (l + r) / 2;
+        build(v, x * 2, l, m);
+        build(v, x * 2 + 1, m + 1, r);
+        push_up(x);
+    }
+
+    SegTreeLazyRangeAdd(vector<T> v) {
         n = v.size();
         n4 = n * 4;
-        tree = vector<T>(n4, 0);
-        lazy = vector<T>(n4, 0);
-        arr = &v;
-        end = n - 1;
-        root = 1;
-        build(0, end, 1);
-        arr = nullptr;
+        tree(n4);
+        build(v);
     }
 
-    void show(int p, int depth = 0) {
-        if (p > n4 || tree[p] == 0) return;
-        show(p * 2, depth + 1);
-        for (int i = 0; i < depth; ++i) putchar('\t');
-        printf("%d:%d\n", tree[p], lazy[p]);
-        show(p * 2 + 1, depth + 1);
-    }
 
-    T range_sum(int l, int r) { return range_sum(l, r, 0, end, root); }
-
-    void range_add(int l, int r, int val) { range_add(l, r, val, 0, end, root); }
 };
