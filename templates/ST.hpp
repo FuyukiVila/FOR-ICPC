61b8cbeb3bff7e360df5fa8db918e5764720fd45
FuyukiVila <1642421711@qq.com>
2024-07-27 15:07:25 +0800

    2024.7.27 ST表更新

--- templates/ST.hpp
+++ templates/ST.hpp
@@ -4,7 +4,7 @@
 #include "vector"
 
 // SpareTable<ElemType, CompareType> ElemType: 元素类型 CompareType: 比较函数, 默认为greater<>
-template<class T, class Cmp = std::greater<T> >
+template<class T = long long, class Cmp = std::greater<T> >
 struct SparseTable {
 private:
     std::vector<std::vector<T> > st;
