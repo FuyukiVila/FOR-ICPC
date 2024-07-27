8248f26b25ec23f0b28846b2645b31bfae35cd12
FuyukiVila <1642421711@qq.com>
2024-07-27 15:06:55 +0800

    2024.7.27 线段树接口实现

--- templates/线段树.hpp
+++ templates/线段树.hpp
@@ -1,19 +1,22 @@
-#ifndef __SEG_TREE_HPP
-#define __SEG_TREE_HPP
+#ifndef __ISEGMENT_TREE_HPP
+#define __ISEGMENT_TREE_HPP
 
 #include <vector>
 
-using namespace std;
+template<class T, class V>
+class ISegmentTree {
 
-template<typename T>
-class SegTree {
-private:
+protected:
+    typedef T element_type;
+    typedef V value_type;
+
     struct node {
-        T value{0};
-        T add{0};
-        T change{0};
+        value_type value;
+        element_type add;
+        element_type change;
         bool isChange{false};
-        int l{0}, r{0};
+        size_t id{0};
+        size_t l{0}, r{0};
 
         constexpr bool inRange(int l, int r) {
             return this->l >= l && this->r <= r;
@@ -23,128 +26,177 @@ private:
             return this->l > r || this->r < l;
         }
 
-        constexpr int length() { return this->r - this->l + 1; }
-
-        constexpr int size() { return length(); }
+        constexpr size_t size() { return r - l + 1; }
     };
 
-    vector<node> tree;
+    constexpr static size_t ls(size_t x) { return 2 * x; }
 
-    void push_down(int x) {
-        // push down操作
-        // 区间求和
+    constexpr static size_t rs(size_t x) { return 2 * x + 1; }
+
+    std::vector<node> tree;
+
+    //you should modify these functions
+    virtual value_type assign_value(const std::vector<element_type> &arr, size_t x);
+
+    virtual value_type merge_value(value_type x, value_type y);
+
+    virtual void add_value(size_t x, element_type add);
+
+    virtual void change_value(size_t x, element_type change);
+
+    void add_tag(size_t x, element_type add) {
+        add_value(x, add);
+        tree[x].add += add;
+    }
+
+    void change_tag(size_t x, element_type change) {
+        change_value(x, change);
+        tree[x].change = change;
+        tree[x].isChange = true;
+        tree[x].add = 0;
+    }
+
+    void push_down(size_t x) {
         if (tree[x].isChange) {
-            tree[x * 2].value = tree[x].change * tree[x * 2].size();
-            tree[x * 2 + 1].value = tree[x].change * tree[x * 2 + 1].size();
-            tree[x * 2].change = tree[x * 2 + 1].change = tree[x].change;
-            tree[x * 2].isChange = tree[x * 2 + 1].isChange = true;
+            change_tag(ls(x), tree[x].change);
+            change_tag(rs(x), tree[x].change);
+            tree[x].isChange = false;
+            tree[x].change = 0;
         }
         if (tree[x].add) {
-            tree[x * 2].value += tree[x].add * tree[x * 2].size();
-            tree[x * 2 + 1].value += tree[x].add * tree[x * 2 + 1].size();
-            tree[x * 2].add += tree[x].add;
-            tree[x * 2 + 1].add += tree[x].add;
+            add_tag(ls(x), tree[x].add);
+            add_tag(rs(x), tree[x].add);
+            tree[x].add = 0;
         }
-        // 区间求max
-        //        if (tree[x].change) {
-        //            tree[x * 2].value = tree[x].change;
-        //            tree[x * 2 + 1].value = tree[x].change;
-        //            tree[x * 2].change = tree[x * 2 + 1].change =
-        //            tree[x].change; tree[x * 2].isChange = tree[x * 2 +
-        //            1].isChange = true;
-        //        }
-        //        if (tree[x].add) {
-        //            tree[x * 2].value += tree[x].add;
-        //            tree[x * 2 + 1].value += tree[x].add;
-        //            tree[x * 2].add += tree[x].add;
-        //            tree[x * 2 + 1].add += tree[x].add;
-        //        }
-
-        tree[x].isChange = tree[x].change = tree[x].add = 0;
     }
 
-    void push_up(int x) {
-        // push up操作
-        // 区间求和
-        tree[x].value = tree[2 * x].value + tree[2 * x + 1].value;
-        //        区间求max
-        //        tree[x].value = max(tree[2 * x].value, tree[2 * x + 1].value);
+    void push_up(size_t x) {
+        tree[x].value = merge_value(tree[ls(x)].value, tree[rs(x)].value);
     }
 
-    void build(const vector<T> &arr, int x, int l, int r) {
+    void build(const std::vector<element_type> &arr, size_t x, size_t l, size_t r) {
+        tree[x].id = x;
         tree[x].l = l, tree[x].r = r;
         if (l == r) {
-            tree[x].value = arr[l];
+            // how to assign value
+            tree[x].value = assign_value(arr, l);
             return;
         }
-        int m = (l + r) >> 1;
-        build(arr, x * 2, l, m);
-        build(arr, x * 2 + 1, m + 1, r);
+        size_t m = (l + r) >> 1;
+        build(arr, ls(x), l, m);
+        build(arr, rs(x), m + 1, r);
         push_up(x);
     }
 
 public:
 
-    SegTree() = default;
+    ISegmentTree() = default;
 
-    explicit SegTree(const vector<T> &arr) {
-        int n = arr.size() - 1;
-        tree.resize(n * 4 + 5);
-        build(arr, 1, 1, n);
+    explicit ISegmentTree(const std::vector<T> &arr) {
+        tree.resize(arr.size() * 4 + 5);
+        build(arr, 1, 1, arr.size() - 1);
     }
 
     // 区间加
-    void add(int l, int r, T val, int x = 1) {
-        if (tree[x].outOfRange(l, r)) {
-            return;
-        }
+    void add(size_t l, size_t r, T val, size_t x = 1) {
         if (tree[x].inRange(l, r)) {
-            tree[x].add += val;
-            // 区间求和
-            tree[x].value += val * tree[x].size();
-            // 区间求Max
-            //  tree[x].value += val;
+            add_tag(x, val);
             return;
         }
         push_down(x);
-        add(l, r, val, x * 2);
-        add(l, r, val, x * 2 + 1);
+        if (!tree[ls(x)].outOfRange(l, r))add(l, r, val, ls(x));
+        if (!tree[rs(x)].outOfRange(l, r))add(l, r, val, rs(x));
         push_up(x);
     }
 
     // 区间修改
-    void change(int l, int r, T val, int x = 1) {
-        if (tree[x].outOfRange(l, r)) {
-            return;
-        }
+    void change(size_t l, size_t r, T val, size_t x = 1) {
         if (tree[x].inRange(l, r)) {
-            tree[x].isChange = true;
-            tree[x].add = 0;
-            tree[x].change = val;
-            // 区间求和
-            tree[x].value = val * tree[x].size();
-            // 区间求Max
-            //  tree[x].value = val;
+            change_tag(x, val);
             return;
         }
         push_down(x);
-        change(l, r, val, x * 2);
-        change(l, r, val, x * 2 + 1);
+        if (!tree[ls(x)].outOfRange(l, r))change(l, r, val, ls(x));
+        if (!tree[rs(x)].outOfRange(l, r))change(l, r, val, rs(x));
         push_up(x);
     }
 
     // 区间查询
-    T query(int l, int r, int x = 1) {
-        if (tree[x].outOfRange(l, r)) {
-            return 0;
-        }
+    value_type query(size_t l, size_t r, size_t x = 1) {
         if (tree[x].inRange(l, r)) {
             return tree[x].value;
         }
         push_down(x);
-        return query(l, r, x * 2) + query(l, r, x * 2 + 1);
-        //        return max(query(l, r, x * 2), query(l, r, x * 2 + 1));
+        if (tree[ls(x)].outOfRange(l, r))return query(l, r, rs(x));
+        if (tree[rs(x)].outOfRange(l, r))return query(l, r, ls(x));
+        return merge_value(query(l, r, ls(x)), query(l, r, rs(x)));
     }
 };
 
 #endif
+
+class SegmentSumTree : public ISegmentTree<long long, long long> {
+private:
+    value_type assign_value(const std::vector<element_type> &arr, size_t x) override {
+        return arr[x];
+    }
+
+    value_type merge_value(value_type x, value_type y) override {
+        return x + y;
+    }
+
+    void add_value(size_t x, value_type val) override {
+        tree[x].value += val * tree[x].size();
+    }
+
+    void change_value(size_t x, value_type val) override {
+        tree[x].value = val * tree[x].size();
+    }
+
+public:
+    SegmentSumTree(const std::vector<element_type> &arr) : ISegmentTree(arr) {}
+};
+
+class SegmentMaxTree : public ISegmentTree<long long, long long> {
+private:
+    value_type assign_value(const std::vector<element_type> &arr, size_t x) override {
+        return arr[x];
+    }
+
+    value_type merge_value(value_type x, value_type y) override {
+        return std::max(x, y);
+    }
+
+    void add_value(size_t x, value_type val) override {
+        tree[x].value += val;
+    }
+
+    void change_value(size_t x, value_type val) override {
+        tree[x].value = val;
+    }
+
+public:
+    SegmentMaxTree(const std::vector<element_type> &arr) : ISegmentTree(arr) {}
+};
+
+class SegmentMinTree : public ISegmentTree<long long, long long> {
+private:
+    value_type assign_value(const std::vector<element_type> &arr, size_t x) override {
+        return arr[x];
+    }
+
+    value_type merge_value(value_type x, value_type y) override {
+        return std::min(x, y);
+    }
+
+    void add_value(size_t x, value_type val) override {
+        tree[x].value += val;
+    }
+
+    void change_value(size_t x, value_type val) override {
+        tree[x].value = val;
+    }
+
+public:
+    SegmentMinTree(const std::vector<element_type> &arr) : ISegmentTree(arr) {}
+};
