3fb9e52342e2bd55b104786365ce69f4401e6be9
FuyukiVila <1642421711@qq.com>
2024-10-17 19:07:06 +0800

    update

--- codeforces/contest2025/D.cpp
+++ codeforces/contest2025/D.cpp
@@ -120,12 +120,14 @@
 
 #define dbg(x...) do { std::cout << #x << " -> "; err(x); } while (0)
 
-void err() { std::cout << std::endl; }
+void err() {
+	std::cout << std::endl;
+}
 
 template<class T, class... Ts>
 void err(T arg, Ts &...args) {
-    std::cout << arg << ' ';
-    err(args...);
+	std::cout << arg << ' ';
+	err(args...);
 }
 
 #else
@@ -141,62 +143,62 @@ constexpr int INF = 0x3f3f3f3f;
 ll mod = 0;
 
 inline ll qpow(ll _a, ll _n, ll _mod = mod) {
-    ll ans = 1;
-    while (_n) {
-        if (_n & 1)
-            ans *= _a;
-        _n >>= 1;
-        _a *= _a;
-        if (_mod > 0) {
-            ans %= _mod;
-            _a %= _mod;
-        }
-    }
-    return ans;
+	ll ans = 1;
+	while (_n) {
+		if (_n & 1)
+			ans *= _a;
+		_n >>= 1;
+		_a *= _a;
+		if (_mod > 0) {
+			ans %= _mod;
+			_a %= _mod;
+		}
+	}
+	return ans;
 }
 
 #ifdef PRIME
 
 namespace prime {
-    const int N = 1e7 + 100;
-    vector<int> minp(N);
-    vector<int> primes;
-    bitset<N> st;
+	const int N = 1e7 + 100;
+	vector<int> minp(N);
+	vector<int> primes;
+	bitset<N> st;
 
-    void get_primes(int n) {
-        for (int i = 2; i <= n; i++) {
-            if (!st[i])
-                minp[i] = i, primes.emplace_back(i);
-            for (int j = 0; primes[j] * i <= n; j++) {
-                int t = primes[j] * i;
-                st[t] = true;
-                minp[t] = primes[j];
-                if (i % primes[j] == 0)
-                    break;
-            }
-        }
-    }
+	void get_primes(int n) {
+		for (int i = 2; i <= n; i++) {
+			if (!st[i])
+				minp[i] = i, primes.emplace_back(i);
+			for (int j = 0; primes[j] * i <= n; j++) {
+				int t = primes[j] * i;
+				st[t] = true;
+				minp[t] = primes[j];
+				if (i % primes[j] == 0)
+					break;
+			}
+		}
+	}
 }
 
 #endif
 
 
 inline void init() {
-    /*Init Here*/
+	/*Init Here*/
 }
 
 void idol_produce(int testCase) {
-    /*Code Here*/
+	/*Code Here*/
 }
 
 
 signed main() {
-    GKD;
-    init();
-    int T = 1;
+	GKD;
+	init();
+	int T = 1;
 //    cin >> T;
-    for (int i = 1; i <= T; i++) {
-        idol_produce(i);
-    }
-    return 0;
+	for (int i = 1; i <= T; i++) {
+		idol_produce(i);
+	}
+	return 0;
 }
